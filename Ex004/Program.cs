85ed0696c20196432b88a6b54a48e2a533032d98
Kaique <kaique.pconceicao1@senacsp.edu.br>
2025-01-16 19:27:17 -0300

    Adicionar arquivos de projeto.

---
+++ Ex004/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Ex004
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int multiplicador;
+
+            Console.WriteLine("Digite um número");
+            multiplicador = Convert.ToInt32(Console.ReadLine());
+
+            int mult1 = 1;
+            int mult2 = 2;
+            int mult3 = 3;
+            int mult4 = 4;
+            int mult5 = 5;
+            int mult6 = 6;
+            int mult7 = 7;
+            int mult8 = 8;
+            int mult9 = 9;
+            int mult10 = 10;
+
+            Console.WriteLine("A multiplicação de {0} * {1} é = {2}", multiplicador, mult1, multiplicador * mult1);
+            Console.WriteLine("A multiplicação de {0} * {1} é = {2}", multiplicador, mult2, multiplicador * mult2);
+            Console.WriteLine("A multiplicação de {0} * {1} é = {2}", multiplicador, mult3, multiplicador * mult3);
+            Console.WriteLine("A multiplicação de {0} * {1} é = {2}", multiplicador, mult4, multiplicador * mult4);
+            Console.WriteLine("A multiplicação de {0} * {1} é = {2}", multiplicador, mult5, multiplicador * mult5);
+            Console.WriteLine("A multiplicação de {0} * {1} é = {2}", multiplicador, mult6, multiplicador * mult6);
+            Console.WriteLine("A multiplicação de {0} * {1} é = {2}", multiplicador, mult7, multiplicador * mult7);
+            Console.WriteLine("A multiplicação de {0} * {1} é = {2}", multiplicador, mult8, multiplicador * mult8);
+            Console.WriteLine("A multiplicação de {0} * {1} é = {2}", multiplicador, mult9, multiplicador * mult9);
+            Console.WriteLine("A multiplicação de {0} * {1} é = {2}", multiplicador, mult10, multiplicador * mult10);
+        }
+    }
+}
---
+++ Ex005/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Ex005
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            double nota1, nota2, media;
+
+
+            Console.WriteLine("Digite sua primeira nota");
+            nota1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite sua segunda nota");
+            nota2 = Convert.ToInt32(Console.ReadLine());
+
+            media = (nota1 + nota2) / 2;
+
+            Console.WriteLine("A média é:{0}", media);
+        }
+    }
+}
