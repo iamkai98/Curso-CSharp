35e10a1b477d6483e0bdf4d7d1a69165b4350d55
Kaique <kaique.pconceicao1@senacsp.edu.br>
2025-01-16 21:52:27 -0300

    atualização de mais if e mais um else

--- Ex006/Program.cs
+++ Ex006/Program.cs
@@ -4,24 +4,36 @@
     {
         static void Main(string[] args)
         {
-            double nota_final = 50;
+            double nota_final = 40;
             string resultado;
             
             if (nota_final >= 60)
             {
-                resultado = "Aprovado";
+                    if (nota_final >= 90)
+                    {
+                        resultado = "Aprovado com louvor";
+                    }
+                    else
+                    {
+                        resultado = "Aprovado";
+                    }
+                
             }
 
-            else if (nota_final >= 40)
+            else 
             {
-                resultado = "Recuperação";
+                    if (nota_final >= 40)
+                    {
+                    resultado = "Recuperação";
+                    }
+                    else
+                    {
+                    resultado = "Reprovado";
+                    }
             }
 
-            else
-            {
-                resultado = "Reprovado";
-            }
-            Console.WriteLine("O aluno foi {0}", resultado);
+            
+            Console.WriteLine("Nota do aluno: {0} - Resultado: {1}", nota_final, resultado);
         }
     }
 }
