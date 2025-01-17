0fc8e27b6eb86ab29907067d2a4de68a890546be
Kaique <kaique.pconceicao1@senacsp.edu.br>
2025-01-16 22:11:13 -0300

    atualização de bimestres

--- Ex006/Program.cs
+++ Ex006/Program.cs
@@ -4,9 +4,23 @@
     {
         static void Main(string[] args)
         {
-            double nota_final = 40;
+            double nota_final, n1,n2,n3,n4;
             string resultado;
-            
+
+            Console.WriteLine("Digite a primeira nota do aluno");
+            n1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite a segunda nota do aluno");
+            n2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite a terceira nota do aluno");
+            n3 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite a quarta nota do aluno");
+            n4 = Convert.ToDouble(Console.ReadLine());
+
+            nota_final = (n1 + n2 + n3 + n4) / 4;
+
             if (nota_final >= 60)
             {
                     if (nota_final >= 90)
